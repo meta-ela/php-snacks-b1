8227bcced6ebc3fed5ca9a0ea328d29bfc18d4bf
meta-ela <anxhelameta@live.it>
2022-07-08 20:06:35 +0200

    Adds snack4 array with random numbers

---
+++ snack4.php
@@ -0,0 +1,31 @@
+<!-- 
+## Snack 4
+Creare un array con 15 numeri casuali, 
+tenendo conto che l’array non dovrà contenere 
+lo stesso numero più di una volta
+
+-->
+
+<?php
+$random_number_array = range(1, 100);
+shuffle($random_number_array );
+$random_number_array = array_slice($random_number_array ,0,15);
+
+var_dump($random_number_array);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <title>Snack4</title>
+</head>
+<body>
+
+</body>
+</html>
